14cd8d13c75f7c29cac2893fd06c7ff16de109c4
Pedro dos Santos Borges <56660097+peuborg@users.noreply.github.com>
2024-08-14 19:54:52 -0300

    início aula 02

--- Aula01/Aula01_Exec02.java
+++ Aula01/Aula01_Exec02.java
@@ -4,10 +4,11 @@ public class Aula01_Exec02 {
     public static void main(String args[]) {
         //Imprima a soma de 500 até 800
         int soma = 0;
-        for(int i = 500; i<= 800; i++){
+        for(int i = 500; i <= 800; i++){
             soma += i;
+            //System.out.println(i + " soma parcial = " + soma);
         }
-        System.out.println("Somatatório = " + soma);
+        System.out.println("Somatório dos números 500 até 800 = " + soma);
     }
     
 }
