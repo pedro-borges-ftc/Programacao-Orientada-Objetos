409f28300a1eba2ed06ea3a17a3ea768db4e9d98
Pedro dos Santos Borges <56660097+peuborg@users.noreply.github.com>
2024-10-15 10:16:37 -0300

    Add files via upload

---
+++ Aula08/Pessoas/Pessoa.java
@@ -0,0 +1,36 @@
+package Pessoas;
+public class Pessoa{
+    //Atributos
+    private String nome;
+    private String endereco;
+    
+    //Construtores
+    Pessoa(){
+        this.nome = "";
+        this.endereco = "";
+    }
+     Pessoa(String n, String e){
+        this.nome = n;
+        this.endereco = e;
+    }
+    //Gets
+    public String getNome(){
+        return this.nome;
+    }
+    public String getEndereco(){
+        return this.endereco;
+    }
+    //Sets
+    public void setNome(String n){
+        this.nome = n;
+    }
+    public void setEndereco(String e){
+        this.endereco = e;
+    }
+    //métodos
+    public String imprimirPessoa(){
+        String resposta = "Nome: " + this.nome + "\n";
+        resposta += "Endereco: " + this.endereco + "\n";
+        return resposta;
+    }
+}
---
+++ Aula08/Pessoas/PessoaJuridica.java
@@ -0,0 +1,27 @@
+package Pessoas;
+public class PessoaJuridica extends Pessoa{
+    //Atributos
+    private String cnpj;
+    //Construtores
+    PessoaJuridica(){
+        super();   //aqui é chamado o construtor da super classe - Pessoa()
+        this.cnpj = "";
+    }
+    PessoaJuridica(String n, String e, String c){
+        super(n,e);   //aqui é chamado o construtor da super classe - Pessoa(nome,endereco)
+        this.cnpj = c;
+    }
+    //Gets e Sets
+    public String getCnpj(){
+        return this.cnpj;
+    }
+    public void setCnpj(String c){
+        this.cnpj = c;
+    }
+    //métodos
+    public String imprimirPessoaJuridica(){
+        String resposta = this.imprimirPessoa();
+        resposta += "CNPJ: " + this.cnpj + "\n";
+        return resposta;
+    }
+}
